78e39165527be9cf88be0d68a7839d67c3ee912f
Junior Santos <juniorsnts123@gmail.com>
2020-03-31 18:35:36 -0300

    atualizado espera de carregamento

--- instagram.py
+++ instagram.py
@@ -4,11 +4,11 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 import time
 credentials = {
-    "username": "",
-    "password": "",
+    "username": "96991190883",
+    "password": "qwe19as3",
 }
 
-userList = ["juniordev.js"]
+userList = ["thediego_st"]
 driver = webdriver.Chrome("/home/juniordev/Downloads/chromedriver")
 driver.implicitly_wait(10)
 
@@ -21,14 +21,13 @@ def open_instagram():
     password = driver.find_element_by_name("password")
     password.send_keys(credentials["password"])
     password.send_keys(Keys.RETURN)
-    time.sleep(3)
+    time.sleep(6)
     for user in userList:
-        driver.get("http://instagram.com/{}/?hl=pt-br".format(user))
+        driver.get("http://instagram.com/{}/".format(user))
         fotos = driver.find_elements_by_css_selector('article > div img[decoding="auto"]')
         time.sleep(3)
         for foto in fotos:
             ActionChains(driver).move_to_element(foto).click().perform()
             time.sleep(1)
-            curtida = driver.find_element('span[aria-label="Curtir"][class="glyphsSpriteHeart__outline__24__grey_9 u-__7"]')
 
 open_instagram()
